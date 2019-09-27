022cd339d23f2ab8227c4a303d9609bd1f27755e
rusneeawae <rus020260@gmail.com>
2019-09-28 02:42:32 +0700

    commit

---
+++ components/Forecast.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+export default class Forecast extends React.Component {
+    render() {
+        return (
+            <View>
+                <View style={styles.container}>
+                    <Text style={styles.cel}>{this.props.main}</Text>
+                    <Text style={styles.texts}>{this.props.description}</Text>
+                    <View style={styles.layout}>
+                        <Text style={styles.cel}>{this.props.temp}</Text>
+                        <Text style={styles.texts}>°C</Text>
+                        
+                    </View>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: { paddingTop: 25 },
+    texts: {flexDirection: 'row',  textAlign: 'center', paddingTop: 20, color: 'white' },
+    cel: { textAlign: 'center', paddingTop: 15, color: 'white', fontSize: 39 },
+    layout: { alignItems: 'center', justifyContent: 'center' },
+    namejan: { textAlign: 'center', color: 'white', fontSize: 20 }
+});
